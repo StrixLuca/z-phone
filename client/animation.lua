b4f0e1924ddf2182008ab3f39d0807ae44ce8bc6
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2026-04-19 19:58:59 +0200

    qbx

--- client/animation.lua
+++ client/animation.lua
@@ -1,59 +1,89 @@
 local phoneProp = 0
 local phoneModel = joaat("prop_npc_phone_02")
 
-local function LoadAnimation(dict)
-	RequestAnimDict(dict)
-	while not HasAnimDictLoaded(dict) do
-		Wait(1)
-	end
+local function loadAnim(dict)
+    if not lib.requestAnimDict(dict, 10000) then
+        error(("Anim dict '%s' kon niet geladen worden"):format(dict))
+    end
 end
 
-local function CheckAnimLoop()
+local function loadModel(model)
+    if not lib.requestModel(model, 10000) then
+        error(("Model '%s' kon niet geladen worden"):format(model))
+    end
+end
+
+local function checkAnimLoop()
     CreateThread(function()
         while PhoneData.AnimationData.lib and PhoneData.AnimationData.anim do
             local ped = PlayerPedId()
+
             if not IsEntityPlayingAnim(ped, PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3) then
-                LoadAnimation(PhoneData.AnimationData.lib)
-                TaskPlayAnim(ped, PhoneData.AnimationData.lib, PhoneData.AnimationData.anim, 3.0, 3.0, -1, 50, 0, false, false, false)
+                loadAnim(PhoneData.AnimationData.lib)
+                TaskPlayAnim(
+                    ped,
+                    PhoneData.AnimationData.lib,
+                    PhoneData.AnimationData.anim,
+                    3.0, 3.0, -1,
+                    50, 0, false, false, false
+                )
             end
+
             Wait(500)
         end
     end)
 end
 
 function newPhoneProp()
-	deletePhone()
-	RequestModel(phoneModel)
-	while not HasModelLoaded(phoneModel) do
-		Wait(1)
-	end
-	phoneProp = CreateObject(phoneModel, 1.0, 1.0, 1.0, 1, 1, 0)
+    deletePhone()
+
+    loadModel(phoneModel)
+
+    phoneProp = CreateObject(phoneModel, 1.0, 1.0, 1.0, true, true, false)
+
     local ped = PlayerPedId()
-	local bone = GetPedBoneIndex(ped, 28422)
-	if phoneModel == joaat("prop_qb-phone_01") then
-		AttachEntityToEntity(phoneProp, ped, bone, 0.0, 0.0, 0.0, 50.0, 320.0, 50.0, 1, 1, 0, 0, 2, 1)
-	else
-		AttachEntityToEntity(phoneProp, PlayerPedId(), bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
-	end
+    local bone = GetPedBoneIndex(ped, 28422)
+
+    if phoneModel == joaat("prop_qb-phone_01") then
+        AttachEntityToEntity(phoneProp, ped, bone, 0.0, 0.0, 0.0, 50.0, 320.0, 50.0, true, true, false, false, 2, true)
+    else
+        AttachEntityToEntity(phoneProp, ped, bone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, true, true, false, false, 2, true)
+    end
+
+    SetModelAsNoLongerNeeded(phoneModel)
 end
 
 function deletePhone()
-	if phoneProp then
-		DeleteObject(phoneProp)
-		phoneProp = 0
-	end
+    if phoneProp ~= 0 then
+        DeleteObject(phoneProp)
+        phoneProp = 0
+    end
 end
 
 function DoPhoneAnimation(anim)
     local ped = PlayerPedId()
-    local AnimationLib = 'cellphone@'
-    local AnimationStatus = anim
-    if IsPedInAnyVehicle(ped, false) then
-        AnimationLib = 'anim@cellphone@in_car@ps'
-    end
-    LoadAnimation(AnimationLib)
-    TaskPlayAnim(ped, AnimationLib, AnimationStatus, 3.0, 3.0, -1, 50, 0, false, false, false)
-    PhoneData.AnimationData.lib = AnimationLib
-    PhoneData.AnimationData.anim = AnimationStatus
-    CheckAnimLoop()
+    local dict = IsPedInAnyVehicle(ped, false) and 'anim@cellphone@in_car@ps' or 'cellphone@'
+
+    loadAnim(dict)
+
+    TaskPlayAnim(
+        ped,
+        dict,
+        anim,
+        3.0, 3.0, -1,
+        50, 0, false, false, false
+    )
+
+    PhoneData.AnimationData.lib = dict
+    PhoneData.AnimationData.anim = anim
+
+    checkAnimLoop()
+
+    -- Anim dict opschonen wanneer animatie stopt
+    CreateThread(function()
+        while PhoneData.AnimationData.lib do
+            Wait(500)
+        end
+        RemoveAnimDict(dict)
+    end)
 end
